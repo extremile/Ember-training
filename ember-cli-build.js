4d9d5740a5443543e3894bd4faafa4bec7525fcc
extremile <miltonleguizamon@yahoo.fr>
2022-06-21 15:01:46 +0200

    exo

---
+++ ember-cli-build.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const autoprefixer = require('autoprefixer');
+const tailwind = require('tailwindcss');
+
+module.exports = function (defaults) {
+  let app = new EmberApp(defaults, {
+    postcssOptions: {
+      compile: {
+        cacheInclude: [/.*\.(css|scss|hbs)$/, /.tailwind\.config\.js$/],
+        plugins: [
+          {
+            module: autoprefixer,
+            options: {},
+          },
+          {
+            module: tailwind,
+            options: {
+              config: './tailwind.config.js',
+            },
+          },
+        ],
+      },
+    },
+  });
+
+  return app.toTree();
+};
