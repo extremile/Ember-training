dd8e1b1fdcb26f20b3abc1c597ed3a29e64cf3f9
extremile <miltonleguizamon@yahoo.fr>
2022-06-27 11:35:14 +0200

    services, helper

--- app/controllers/songs.js
+++ app/controllers/songs.js
@@ -1,9 +1,24 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { service } from '@ember/service';
 
 export default class SongsController extends Controller {
+  @service favoritesSong;
+
   @action
   deleteSong(song) {
+    if (this.favoritesSong.items.includes(song)) {
+      this.favoritesSong.remove(song);
+    }
     song.destroyRecord();
+  }
+
+  @action
+  toggleFavorite(song) {
+    if (this.favoritesSong.items.includes(song)) {
+      this.favoritesSong.remove(song);
+    } else {
+      this.favoritesSong.add(song);
+    }
   }
 }
