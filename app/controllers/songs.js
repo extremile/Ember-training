d942f0e6a9230a1405132166194741dc74424f70
extremile <miltonleguizamon@yahoo.fr>
2022-06-27 12:14:32 +0200

    queryparams

--- app/controllers/songs.js
+++ app/controllers/songs.js
@@ -1,9 +1,28 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class SongsController extends Controller {
   @service favoritesSong;
+  queryParams = ['name']; // On précise le queryParams qui va exister
+
+  @tracked name = 'asc'; // Valeur par défaut de mon queryParams
+
+  get direction() {
+    // Avoir la prochaine direction de mon queryparams
+    return this.model.query == 'asc' ? 'desc' : 'asc';
+  }
+
+  get filteredSongs() {
+    let songs = this.model.songs;
+
+    if (this.name == 'asc') {
+      return songs.sortBy('title');
+    } else {
+      return songs.sortBy('title').reverse();
+    }
+  }
 
   @action
   deleteSong(song) {
